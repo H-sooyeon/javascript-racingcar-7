916276e2b7112b3602c8b776f1137924e35d780c
H-sooyeon <wndqhr__@naver.com>
2024-11-05 21:10:01 +0900

    refactor: 에러 메시지 및 구분자 상수화

---
+++ src/constants/errorMessage.js
@@ -0,0 +1,9 @@
+export default {
+  INVALID_CAR_NAME:
+    '[ERROR] 자동차 이름은 5자 이하 한글, 영문만 입력 가능합니다.',
+  INVALID_CAR_COUNT: '[ERROR] 자동차는 2대 이상 입력해주세요.',
+  DUPLICATED_CAR_NAME: '[ERROR] 중복된 자동차 이름이 존재합니다.',
+  INVALID_TRY_COUNT:
+    '[ERROR] 시도 횟수는 1 이상 10 이하 정수값을 입력해주세요.',
+  INVALID_NUMBER: '[ERROR] 시도 횟수는 숫자로 입력해주세요.',
+};
---
+++ src/constants/rules.js
@@ -0,0 +1,3 @@
+export default {
+  DELIMITER: ',', // 경주할 자동차 이름을 구분하는 구분자
+};
